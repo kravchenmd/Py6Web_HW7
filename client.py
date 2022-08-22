d69ad73845bfba008ed5f50c44af814c0cb74643
kravchenmd <kravchenmd@gmail.com>
2022-08-22 19:47:20 +0200

    Create client and server (final)

--- client.py
+++ client.py
@@ -1,22 +1,27 @@
 import socket
 
+TCP_IP = '192.168.0.187'  # from `get_ip()` of server
+TCP_PORT = 5000
 
-def client():
-    host = socket.gethostname()
-    port = 5000
 
-    client_socket = socket.socket()
-    client_socket.connect((host, port))
-    message = input('--> ')
-
-    while message.lower().strip() != 'end':
+def run_client(ip: str, port: int):
+    with socket.socket() as sock:
+        server = ip, port
+        client_socket = socket.socket()
+        client_socket.connect(server)
+        print(f"Connection with server @ {server} was established")
+        message = input('--> ')
         client_socket.send(message.encode())
         data = client_socket.recv(1024).decode()
-        print(f'received message: {data}')
-        message = input('--> ')
 
-    client_socket.close()
+        while data != 'OK':
+            data = client_socket.recv(1024).decode()
+            if not data:
+                print("ERROR: connection was closed, but `OK` wasn't received!")
+                return
+        print(data)
+    print(f"Connection with server was closed")
 
 
 if __name__ == '__main__':
-    client()
+    run_client(TCP_IP, TCP_PORT)
--- server.py
+++ server.py
@@ -1,25 +1,64 @@
+import os
 import socket
+from concurrent import futures as cf
+
+# From Stack Overflow (modernized)
+# For Linux
+if os.name != "nt":
+    import fcntl
+    import struct
 
 
-def main():
-    host = socket.gethostname()
-    port = 5000
+    def get_interface_ip(if_name):
+        s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        return socket.inet_ntoa(fcntl.ioctl(s.fileno(), 0x8915,
+                                            struct.pack('256s', if_name[:15]))[20:24])
+
+
+def get_ip():
+    ip = socket.gethostbyname(socket.gethostname())
+
+    # For Linux
+    if ip.startswith("127.") and os.name != "nt":
+        interfaces = ["eth0", "eth1", "eth2", "wlan0", "wlan1", "wifi0", "ath0", "ath1", "ppp0"]
+        for if_name in interfaces:
+            try:
+                ip = get_interface_ip(if_name)
+                break
+            except IOError:
+                pass
+    return ip
+
+
+def run_server(ip, port):
+    def handle(sock: socket.socket, addr: str):
+        print(f'Connection established {addr}')
+        while True:
+            received = sock.recv(1024)
+            if not received:
+                break
+            data = received.decode()
+            print(f'Data received: {data}')
+            sock.send('OK'.encode())
+            print(f'OK was send to {addr}. Connection closed')
+            sock.close()
 
     server_socket = socket.socket()
-    server_socket.bind((host, port))
-    server_socket.listen(2)
-    conn, address = server_socket.accept()
-    print(f'Connection from {address}')
-    while True:
-        data = conn.recv(100).decode()
-
-        if not data:
-            break
-        print(f'received message: {data}')
-        message = input('--> ')
-        conn.send(message.encode())
-    conn.close()
+    server_socket.bind((ip, port))
+    server_socket.listen(5)
+    print(f'Start server @ {server_socket.getsockname()}')
+    with cf.ThreadPoolExecutor(5) as client_pool:
+        try:
+            while True:
+                new_sock, address = server_socket.accept()
+                client_pool.submit(handle, new_sock, address)
+        except KeyboardInterrupt:
+            print(f"Server stopped")
+        finally:
+            server_socket.close()
 
 
 if __name__ == '__main__':
-    main()
+    TCP_IP = get_ip()
+    TCP_PORT = 5000
+    run_server(TCP_IP, TCP_PORT)
